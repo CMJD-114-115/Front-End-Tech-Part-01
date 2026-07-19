b48a6ee02bfa5c588dc687b1c7f0e842af11515e
ThanuraSilva <thanurasilva1989@gmail.com>
2026-07-19 14:51:00 +0530

    Collect from data -1

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Hello");
+
+const userForm = document.getElementById("user-form")
+
+userForm.addEventListener("submit",(e) =>{
+    e.preventDefault()
+    // Get form values
+    const firstName = document.getElementById("first-name").value
+    const lastName = document.getElementById("last-name").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const role = document.getElementById("role").value
+
+    // Insert to a JS Object
+      const collectedFormData = {
+        formFirstName:firstName,
+        formLastName:lastName,
+        formEmail:email,
+        formPassword:password,
+        formRole:role
+      }
+
+    //   Print object 
+    console.log("Form Dataset: ",collectedFormData)
+
+    // To JSON
+        console.log("Form Dataset as JSON: ",JSON.stringify(collectedFormData))
+
+})
