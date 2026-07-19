0b71902d5e9dcd3ccc035938c98369d55fdd76cf
ThanuraSilva <thanurasilva1989@gmail.com>
2026-07-19 13:53:01 +0530

    Data types of JS

---
+++ Intro to JS/dataTypes.js
@@ -0,0 +1,26 @@
+let grade = "Upper"
+let age = 35
+let isDone = true
+let value = null
+
+const student = {
+    name: "Kamal",
+    city: "Colombo"
+}
+
+const cars = ["BMW","Benz","BYD","Toyota"]
+const anoterArray = ["BMW","Benz","BYD","Toyota", 20 ,true]
+
+let address;
+
+console.log("Type of the grade is: ", typeof grade);
+console.log("Type of the age is: ", typeof age);
+console.log("Type of the isDone is: ", typeof isDone);
+console.log("Type of the value is: ", typeof value);
+console.log("Type of the student is: ", typeof student);
+console.log("Type of the cars is: ", typeof cars);
+console.log("Type of the anoterArray is: ", typeof anoterArray);
+console.log("Type of the address is: ", typeof address);
+console.log("Type of the student is: ", typeof student.age);
+
+
