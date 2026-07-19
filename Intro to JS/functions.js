18130629be7f207a85c1a22b620ad69b0bc18353
ThanuraSilva <thanurasilva1989@gmail.com>
2026-07-19 13:39:16 +0530

    JS Functions

---
+++ Intro to JS/functions.js
@@ -0,0 +1,20 @@
+function myFunction(){
+    console.log("Hello My Function");
+}
+myFunction();
+
+const otherMyFunction = () =>{
+     console.log("Hello My Other Function");
+}
+otherMyFunction()
+
+const calc = (num1, num2) =>{
+     console.log("Calc result is: ", num1 + num2);
+}
+calc(20,45)
+
+function myReturnValueFunction(){
+    return "Hello CMJD - 114/115"
+}
+console.log(myReturnValueFunction())
+ 
