304fb1452c2d1526dba069fe92efd7810b637ad6
ThanuraSilva <thanurasilva1989@gmail.com>
2026-07-19 13:14:31 +0530

    Intro to JS and way how to define variables

---
+++ Intro to JS/main.js
@@ -0,0 +1,14 @@
+var name = "Thanura"
+console.log("Name is : ",name);
+
+let city =" Kalutara"
+console.log("City is: ",city )
+
+const email = "thanu@mail.com"
+console.log("Email is: ",email)
+
+name = "Kamal"
+console.log("New Name is : ",name);
+
+email = "kamal@mail.com"
+console.log("New Email is: ",email)
