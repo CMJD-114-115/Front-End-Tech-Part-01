41c10364e34c504271caf5fb533b02ada912dae8
ThanuraSilva <thanurasilva1989@gmail.com>
2026-07-19 14:04:58 +0530

    Assignment Ops

---
+++ Intro to JS/assignmentOps.js
@@ -0,0 +1,11 @@
+let num1= 20; 
+let num2 = 80
+
+console.log("Sum of num1 and mum2 is: ", num1 + num2);
+console.log("Min of num1 and mum2 is: ", num1 - num2);
+console.log("Multi of num1 and mum2 is: ", num1 * num2);
+console.log("Sub of num1 and mum2 is: ", num1 / num2);
+console.log("Remainder of num1 and mum2 is: ", num1 % num2);
+
+ num1 += 10
+ console.log("Short way is: ",num1)
